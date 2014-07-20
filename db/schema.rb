2b8c8685c8c4f53003bfa9076d536722c2eeca42
David Ko <david.ko@damncarousel.com>
2014-07-20 18:14:36 -0400

    Add checkins scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140720215941) do
+ActiveRecord::Schema.define(version: 20140720220900) do
+
+  create_table "checkins", force: true do |t|
+    t.integer  "goal_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "goals", force: true do |t|
     t.datetime "created_at"
