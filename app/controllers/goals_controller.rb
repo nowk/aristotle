91508b3389c1d53ace22fb5b93a66964551d5290
David Ko <david.ko@damncarousel.com>
2014-07-22 08:38:52 -0400

    Goal index page

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -69,6 +69,6 @@ class GoalsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def goal_params
-      params[:goal]
+      params[:goal].permit(:name, :description)
     end
 end
