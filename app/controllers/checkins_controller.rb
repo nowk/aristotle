6e1cb853655d64bc8d311370450966d9b1119dab
David Ko <david.ko@damncarousel.com>
2014-07-23 08:24:09 -0400

    Checkin create

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -2,6 +2,16 @@ class CheckinsController < ApplicationController
   before_action :set_checkin, only: [:destroy]
 
   def create
+    @goal = Goal.find(params[:goal_id])
+    @checkin = @goal.checkins.new
+
+    respond_to do |format|
+      if @checkin.save
+        format.json { render json: { status: 200 } }
+      else
+        format.json { render json: { status: 500 } }
+      end
+    end
   end
 
   def destroy
