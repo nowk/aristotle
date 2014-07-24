056375feb7a4af40f6f2b518e03c7adf00ac0cef
David Ko <david.ko@damncarousel.com>
2014-07-24 17:05:47 -0400

    Routes OCD

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  root 'pages#home'
+
+  # Odds n' Ends
   get 'signup', to: 'users#new', as: 'signup'
   get 'login', to: 'sessions#new', as: 'login'
   get 'logout', to: 'sessions#destroy', as: 'logout'
   get '/users/:user_id/goals/:id/:date_string', to: 'goals#show', as: 'user_goal' 
   get '/users/:user_id/goals/:id', to: 'goals#show', defaults: { date_string: Date.today.strftime('%m%d%Y') }
 
+  # Resources
   resources :prizes
   resources :sessions
   resources :checkins, only: [:create, :destroy]
   resources :users do
     resources :goals, except: :show
   end
-  root 'pages#home'
 
 end
