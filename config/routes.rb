14718db67c50a98888a1740799ce15705452d1ab
David Ko <david.ko@damncarousel.com>
2014-07-25 21:27:57 -0400

    fixing up routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,6 @@
 Rails.application.routes.draw do
   root 'pages#home'
 
-  # Odds n' Ends
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
-  get '/users/:user_id/goals/:id', to: 'goals#show', defaults: { date_string: Date.today.strftime('%m%d%Y') }, as: 'user_goal'
-  get '/users/:user_id/goals/:id/:date_string', to: 'goals#show', as: 'user_goal_with_date' 
-
   # Resources
   resources :prizes
   resources :sessions
@@ -15,5 +8,12 @@ Rails.application.routes.draw do
   resources :users do
     resources :goals, except: :show
   end
+
+  # Odds n' Ends
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get '/users/:user_id/goals/:id', to: 'goals#show', defaults: { date_string: Date.today.strftime('%m%d%Y') }, as: 'goal'
+  get '/users/:user_id/goals/:id/:date_string', to: 'goals#show', as: 'goal_with_date' 
 
 end
