da9eb197ae5e732352a2948d078706bd144254d9
David Ko <david.ko@damncarousel.com>
2014-07-25 21:28:07 -0400

    Goal summary facade

---
+++ app/facades/goal_summary.rb
@@ -0,0 +1,27 @@
+class GoalSummary
+  attr_reader :goal, :description, :name, :date
+
+  def initialize(goal, date_string)
+    @goal = goal
+    @description = @goal.description
+    @name = @goal.name
+    @date = DateTime.strptime(date_string, '%m%d%Y')
+  end
+
+  def checkins
+    @checkins ||= @goal.checkins
+  end
+
+  def todays_checkin
+    @todays_checkin ||= @goal.get_checkin_for(@date).first
+  end
+
+  def todays_checkin_id
+    @todays_checkin_id ||= @todays_checkin.id
+  end
+
+  def goal_id
+    @goal_id ||= @goal.id
+  end
+
+end
