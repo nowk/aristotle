c748d22b74cca63d95cf7387b6e187cb4bb9b29a
David Ko <david.ko@damncarousel.com>
2014-07-27 16:53:18 -0400

    First and last name params for user sign up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -70,6 +70,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:email, :password, :password_confirmation, :time_zone)
+      params.require(:user).permit(:email, :password, :password_confirmation, :time_zone, :first_name, :last_name)
     end
 end
