0e45a99515143c84f0bb770bbabbf4af3a481a7f
David Ko <david.ko@damncarousel.com>
2014-07-30 07:29:39 -0400

    Fixes AT for #4

--- spec/features/user_logs_in_spec.rb
+++ spec/features/user_logs_in_spec.rb
@@ -1,27 +1,21 @@
 require 'rails_helper'
-require 'database_cleaner'
 
 DatabaseCleaner.strategy = :truncation, { only: %w[users] }
 
 feature 'User logs in' do
   before(:each) do
     user = FactoryGirl.create(:user)
-  end
-
-  after(:each) do
-    DatabaseCleaner.clean
+    visit login_path
   end
 
   scenario 'valid email/password takes User to dashboard' do
-    visit '/login'
     fill_in 'email', with: 'john@example.com'
     fill_in 'password', with: 'password'
     click_button 'Login'
-    expect(page).to have_content 'My Goals'
+    expect(page).to have_selector '.dashboard'
   end
 
   scenario 'invalid email/password display error message' do
-    visit '/login'
     fill_in 'email', with: 'baduser@example.com'
     fill_in 'password', with: 'password'
     click_button 'Login'
