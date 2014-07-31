58be2c9c2964f909c39a0fa5cdbe1020bb526973
David Ko <david.ko@damncarousel.com>
2014-07-31 08:46:13 -0400

    Fix DB cleaner config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -20,9 +20,7 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # If you are not using ActiveRecord, you can remove this line.
 ActiveRecord::Migration.maintain_test_schema!
 
-DatabaseCleaner.strategy = :truncation, { only: %w[users] }
-
-
+DatabaseCleaner.strategy = :truncation
 
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods  
@@ -40,10 +38,25 @@ RSpec.configure do |config|
   #
   # The different available types are documented in the features, such as in
   # https://relishapp.com/rspec/rspec-rails/docs
+
+  config.use_transactional_fixtures = false
+
   config.infer_spec_type_from_file_location!
 
+  config.before(:suite) do
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
   config.before(:each) do
-    DatabaseCleaner.clean
+    DatabaseCleaner.strategy = :transaction
+  end
+
+  config.before(:each, js: true) do
+    DatabaseCleaner.strategy = :truncation
+  end
+
+  config.before(:each) do 
+    DatabaseCleaner.start
   end
 
   config.after(:each) do
