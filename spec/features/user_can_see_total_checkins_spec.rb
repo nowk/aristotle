f0682fe57498aad6f0cdd23a99487033e79b51d9
David Ko <david.ko@damncarousel.com>
2014-08-05 08:28:51 -0400

    Fix re: https://github.com/nycdavid/aristotle/commit/c1b643a33e6255a01b97caaa893e81c51e67d90c#commitcomment-7255351

--- spec/features/user_can_see_total_checkins_spec.rb
+++ spec/features/user_can_see_total_checkins_spec.rb
@@ -7,7 +7,7 @@ feature 'User goes into goal index and sees total checkins for a goal' do
     FactoryGirl.create :checkin, { truncated_date: convert_to_s(2.days.ago) }
   end
 
-  scenario 'User checks in several times' do
+  scenario 'User has already checked in several times' do
     visit user_goals_path(@user.id)
     el = page.find("tr[data-goal-id='#{@goal.id}']").find('.total-checkins')
     expect(el).to have_text(3)
