5f92405888ad3e70cdb6bc436d775a55e317b485
David Ko <david.ko@damncarousel.com>
2014-08-06 07:23:56 -0400

    Selector is too brittle

--- spec/features/user_can_see_total_checkins_spec.rb
+++ spec/features/user_can_see_total_checkins_spec.rb
@@ -9,7 +9,7 @@ feature 'User goes into goal index and sees total checkins for a goal' do
 
   scenario 'User has already checked in several times' do
     visit user_goals_path(@user.id)
-    el = page.find("tr[data-goal-id='#{@goal.id}']").find('.total-checkins')
+    el = page.find("*[data-goal-id='#{@goal.id}']").find('.total-checkins')
     expect(el).to have_text(3)
   end
 end
