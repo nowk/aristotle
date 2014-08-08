fa997237dcb024eaa2097029b1c71eb81bd94cec
David Ko <david.ko@damncarousel.com>
2014-08-08 07:55:04 -0400

    User updates goal acceptance tests

---
+++ spec/features/user_updates_goal_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+feature 'User updates goal' do
+  before :each do
+    visit edit_user_goal_path(@user.id, @goal.id)
+  end
+
+  scenario 'User updates goal with valid info' do
+    fill_in 'goal[name]', with: 'BarBaz'
+    fill_in 'goal[description]', with: 'This is just sample description.'
+    click_button 'Save Goal'
+    expect(page).to have_selector '.notice'
+  end
+
+  scenario 'User updates goal with invalid info' do
+    fill_in 'goal[name]', with: ''
+    click_button 'Save Goal'
+    expect(page).to have_selector '.alert'
+  end
+end
