3fae6749ce6b8c7e63b6b8bb2d59458d54b0968e
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-08-12 00:14:54 -0400

    Pop @goal back in there

--- app/controllers/checkins_controller.rb
+++ app/controllers/checkins_controller.rb
@@ -15,10 +15,11 @@ class CheckinsController < ApplicationController
 
   def destroy
     @checkin = Checkin.find(params[:id])
+    @goal = @checkin.goal
 
     respond_to do |format|
       if @checkin.destroy
-        format.html { redirect_to goal_with_date_path(current_user.id, @checkin.goal_id, @checkin.truncated_date), notice: 'Checked out for today.' }
+        format.html { redirect_to goal_with_date_path(current_user.id, @goal, @checkin.truncated_date), notice: 'Checked out for today.' }
         format.json { render json: { status: 200, total_checkins: @goal.checkins.count } }
       else
         format.json { render json: { status: 500, total_checkins: @goal.checkins.count } }
