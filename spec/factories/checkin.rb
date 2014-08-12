31f0335d0a4e5454eeb0e7d86164eeb6605f695d
Yung Hwa Kwon <yung.kwon@damncarousel.com>
2014-08-11 23:55:01 -0400

    Refactor checkin spec

--- spec/factories/checkin.rb
+++ spec/factories/checkin.rb
@@ -3,4 +3,8 @@ FactoryGirl.define do
     goal_id '1' 
     truncated_date { DateTime.now.strftime('%m%d%Y') }
   end
+
+  factory :newcheckin, class: Checkin do
+    truncated_date { DateTime.now.strftime('%m%d%Y') }
+  end
 end
